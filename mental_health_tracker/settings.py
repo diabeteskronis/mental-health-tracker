449ba808d981ea5cea722a30e3babee123510052
diabeteskronis <andharu.achmadsyah@gmail.com>
2024-09-11 14:17:00 +0700

    fixed settings.py for deployment

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -123,6 +123,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://<URL PWS KAMU>", "https://https://andharu-hanif-mentalhealthtracker.pbp.cs.ui.ac.id/"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://andharu-hanif-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://andharu-hanif-mentalhealthtracker.pbp.cs.ui.ac.id/"]
 
 
